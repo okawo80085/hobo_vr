ebe6f8db0ea187d127b815cbdae24f11eb4298a2
okawo <okawo.198@gmail.com>
2020-05-24 22:11:01 +0300

    im leaving for lunch...

---
+++ examples/less_crude_demo.py
@@ -0,0 +1,175 @@
+"""
+Shadow mapping example from:
+https://www.opengl-tutorial.org/intermediate-tutorials/tutorial-16-shadow-mapping/
+"""
+import math
+from pathlib import Path
+from pyrr import Matrix44, matrix44, Vector3
+import numpy as np
+
+
+import moderngl
+import moderngl_window
+from moderngl_window import geometry
+
+from base import CameraWindow
+
+
+class ShadowMapping(CameraWindow):
+    title = "Shadow Mapping"
+    resource_dir = (Path(__file__) / '../../resources').resolve()
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.camera.projection.update(near=1, far=200)
+        self.wnd.mouse_exclusivity = True
+
+        # Offscreen buffer
+        offscreen_size = 1024, 1024
+        self.offscreen_depth = self.ctx.depth_texture(offscreen_size)
+        self.offscreen_depth.compare_func = ''
+        self.offscreen_depth.repeat_x = False
+        self.offscreen_depth.repeat_y = False
+        self.offscreen_color = self.ctx.texture(offscreen_size, 4)
+
+        self.offscreen = self.ctx.framebuffer(
+            color_attachments=[self.offscreen_color],
+            depth_attachment=self.offscreen_depth,
+        )
+
+        # Scene geometry
+        self.hmd = geometry.cube(size=(5, 5, 1), center=(0, -6, 0))
+        self.hmd2 = geometry.cube(size=(2, 2, 1), center=(0, -6, -2))
+
+        self.handL = geometry.cube(size=(2, 2, 5), center=(0, 0, 0))
+        self.handL2 = geometry.cube(size=(1, 1, 1), center=(2, 0, 0))
+        self.handL3 = geometry.cube(size=(1, 1, 1), center=(0, 0, 4))
+
+        self.handR = geometry.cube(size=(2, 2, 5), center=(0, 2.5, 0))
+        self.handR2 = geometry.cube(size=(1, 1, 1), center=(2, 2.5, 0))
+        self.handR3 = geometry.cube(size=(1, 1, 1), center=(0, 2.5, 4))
+
+        # self.sun = geometry.sphere(radius=1.0)
+
+        # Debug geometry
+        self.offscreen_quad = geometry.quad_2d(size=(0.5, 0.5), pos=(0.75, 0.75))
+        self.offscreen_quad2 = geometry.quad_2d(size=(0.5, 0.5), pos=(0.25, 0.75))
+
+        # Programs
+        self.raw_depth_prog = self.load_program('programs/shadow_mapping/raw_depth.glsl')
+
+        # self.basic_light = self.load_program('programs/shadow_mapping/directional_light.glsl')
+        # self.basic_light['shadowMap'].value = 0
+        # self.basic_light['color'].value = 1.0, 1.0, 1.0, 1.0
+
+
+        self.shadowmap_program = self.load_program('programs/shadow_mapping/shadowmap.glsl')
+        self.basic_lightL = self.load_program('programs/shadow_mapping/directional_light.glsl')
+        self.basic_lightL['shadowMap'].value = 0
+        self.basic_lightL['color'].value = 0.0, 1.0, 0.0, 1.0
+
+        self.basic_lightR = self.load_program('programs/shadow_mapping/directional_light.glsl')
+        self.basic_lightR['shadowMap'].value = 0
+        self.basic_lightR['color'].value = 0.0, 0.0, 1.0, 1.0
+
+        self.basic_lightHmd = self.load_program('programs/shadow_mapping/directional_light.glsl')
+        self.basic_lightHmd['shadowMap'].value = 0
+        self.basic_lightHmd['color'].value = 1.0, 0.0, 0.0, 1.0
+
+
+        self.texture_prog = self.load_program('programs/texture.glsl')
+        self.texture_prog['texture0'].value = 0
+
+        self.sun_prog = self.load_program('programs/cube_simple.glsl')
+        self.sun_prog['color'].value = 1, 1, 0, 1
+        self.lightpos = 0, 0, 0
+        time = 2
+        self.lightpos = Vector3((math.sin(time) * 20, 5, math.cos(time) * 20), dtype='f4')
+
+    def render(self, time, frametime):
+        self.ctx.enable_only(moderngl.DEPTH_TEST | moderngl.CULL_FACE)
+        scene_pos = Vector3((0, -5, -32), dtype='f4')
+
+        # --- PASS 1: Render shadow map
+        self.offscreen.clear()
+        self.offscreen.use()
+
+        depth_projection = Matrix44.orthogonal_projection(-20, 20, -20, 20, -20, 40, dtype='f4')
+        depth_view = Matrix44.look_at(self.lightpos, (0, 0, 0), (0, 1, 0), dtype='f4')
+        depth_mvp = depth_projection * depth_view
+        self.shadowmap_program['mvp'].write(depth_mvp)
+
+        self.handL.render(self.shadowmap_program)
+        self.handL2.render(self.shadowmap_program)
+        self.handL3.render(self.shadowmap_program)
+
+        self.handR.render(self.shadowmap_program)
+        self.handR2.render(self.shadowmap_program)
+        self.handR3.render(self.shadowmap_program)
+
+        self.hmd.render(self.shadowmap_program)
+        self.hmd2.render(self.shadowmap_program)
+
+        # --- PASS 2: Render scene to screen
+        self.wnd.use()
+        # self.basic_light['m_proj'].write(self.camera.projection.matrix)
+        # self.basic_light['m_camera'].write(self.camera.matrix)
+        # self.basic_light['m_model'].write(Matrix44.from_translation(scene_pos, dtype='f4') * Matrix44.from_y_rotation(time, dtype='f4'))
+        bias_matrix = Matrix44(
+            [[0.5, 0.0, 0.0, 0.0],
+             [0.0, 0.5, 0.0, 0.0],
+             [0.0, 0.0, 0.5, 0.0],
+             [0.5, 0.5, 0.5, 1.0]],
+            dtype='f4',
+        )
+        # self.basic_light['m_shadow_bias'].write(matrix44.multiply(depth_mvp, bias_matrix))
+        # self.basic_light['lightDir'].write(self.lightpos)
+
+        self.offscreen_depth.use(location=0)
+
+        self.basic_lightL['m_proj'].write(self.camera.projection.matrix)
+        self.basic_lightL['m_camera'].write(self.camera.matrix)
+        self.basic_lightL['m_shadow_bias'].write(matrix44.multiply(depth_mvp, bias_matrix))
+        self.basic_lightL['lightDir'].write(self.lightpos)
+        self.basic_lightL['m_model'].write(Matrix44.from_translation(scene_pos, dtype='f4') * Matrix44.from_y_rotation(time, dtype='f4'))
+
+        self.basic_lightR['m_proj'].write(self.camera.projection.matrix)
+        self.basic_lightR['m_camera'].write(self.camera.matrix)
+        self.basic_lightR['m_shadow_bias'].write(matrix44.multiply(depth_mvp, bias_matrix))
+        self.basic_lightR['lightDir'].write(self.lightpos)
+        self.basic_lightR['m_model'].write(Matrix44.from_translation(scene_pos, dtype='f4') * Matrix44.from_y_rotation(time, dtype='f4'))
+
+        self.basic_lightHmd['m_proj'].write(self.camera.projection.matrix)
+        self.basic_lightHmd['m_camera'].write(self.camera.matrix)
+        self.basic_lightHmd['m_shadow_bias'].write(matrix44.multiply(depth_mvp, bias_matrix))
+        self.basic_lightHmd['lightDir'].write(self.lightpos)
+        self.basic_lightHmd['m_model'].write(Matrix44.from_translation(scene_pos, dtype='f4') * Matrix44.from_y_rotation(time, dtype='f4'))
+
+        self.handL.render(self.basic_lightL)
+        self.handL2.render(self.basic_lightL)
+        self.handL3.render(self.basic_lightL)
+
+        self.handR.render(self.basic_lightR)
+        self.handR2.render(self.basic_lightR)
+        self.handR3.render(self.basic_lightR)
+
+        self.hmd.render(self.basic_lightHmd)
+        self.hmd2.render(self.basic_lightHmd)
+
+        # # Render the sun position
+        # self.sun_prog['m_proj'].write(self.camera.projection.matrix)
+        # self.sun_prog['m_camera'].write(self.camera.matrix)
+        # self.sun_prog['m_model'].write(Matrix44.from_translation(self.lightpos + scene_pos, dtype='f4'))
+        # self.sun.render(self.sun_prog)
+
+        # --- PASS 3: Debug ---
+        # self.ctx.enable_only(moderngl.NOTHING)
+        self.offscreen_depth.use(location=0)
+        self.offscreen_quad.render(self.raw_depth_prog)
+        # self.offscreen_color.use(location=0)
+        # self.offscreen_quad2.render(self.texture_prog)
+
+
+if __name__ == '__main__':
+    moderngl_window.run_window_config(ShadowMapping)
+    print ('lol')
