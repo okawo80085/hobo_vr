164a58bb4ce691e6cf27c745b964b841a1167d33
okawo80085 <okawo.198@gmail.com>
2020-06-07 21:25:39 +0300

    small fix

--- virtualreality/util/utilz.py
+++ virtualreality/util/utilz.py
@@ -132,9 +132,10 @@ def strings_share_characters(str1: str, str2: str) -> bool:
 
 def get_numbers_from_text(text, separator="\t"):
     """Get a list of number from a string of numbers seperated by :separator:[default: "\t"]."""
-    if isinstance(text, bytearray) or isinstance(text, bytes):
-        text = text.decode("utf-8")
     try:
+        if isinstance(text, bytearray) or isinstance(text, bytes):
+            text = text.decode("utf-8")
+
         if strings_share_characters(text.lower(), "qwrtyuiopsasdfghjklzxcvbnm><*[]{}()") or len(text) == 0:
             return []
 
