0d3e11a3a4845c8e8b35dbb2ab3ff49412c30919
okawo <okawo.198@gmail.com>
2020-06-21 19:24:27 +0300

    i changed the code on my imus
    they now directly send orientation in quaternions, but it still drifts it yaw

--- virtualreality/trackers/color_tracker.py
+++ virtualreality/trackers/color_tracker.py
@@ -145,173 +145,147 @@ class Poser(templates.PoserTemplate):
         past_velocity = [0, 0, 0]
         velocity_until_reset = 0
         temp_for_offz = {"x": 0, "y": 0, "z": 0}
-        while self.coro_keep_alive["serial_listener_2"][0]:
-            try:
-                yaw_offset = 0
-                with serial.Serial(self.serialPaths["green"], 115200, timeout=1 / 4) as ser:
-                    with serial.threaded.ReaderThread(ser, u.SerialReaderFactory) as protocol:
-                        for _ in range(10):
-                            protocol.write_line("nut")
-                            await asyncio.sleep(1)
 
-                        while self.coro_keep_alive["serial_listener_2"][0]:
-                            try:
-                                gg = u.get_numbers_from_text(protocol.last_read)
+        my_off = Quaternion()
+        with serial.Serial(self.serialPaths["green"], 115200, timeout=1 / 4) as ser:
+            with serial.threaded.ReaderThread(ser, u.SerialReaderFactory) as protocol:
+                for _ in range(10):
+                    protocol.write_line("nut")
+                    await asyncio.sleep(1)
 
-                                if len(gg) > 0:
-                                    (y, p, r, az, ax, ay, trgr, grp, util, sys, menu, padClk, padY, padX,) = gg
+                while self.coro_keep_alive["serial_listener_2"][0]:
+                    try:
+                        gg = u.get_numbers_from_text(protocol.last_read)
 
-                                    if velocity_until_reset < 20:
+                        if len(gg) > 0:
+                            (w, x, y, z, trgr, grp, util, sys, menu, padClk, padY, padX,) = gg
 
-                                        u.rotate_y(
-                                            {"": temp_for_offz}, math.radians(-yaw_offset),
-                                        )
-                                        past_velocity = [
-                                            temp_for_offz["x"],
-                                            temp_for_offz["y"],
-                                            temp_for_offz["z"],
-                                        ]
-                                        temp_for_offz["x"] = round(
-                                            past_velocity[0]
-                                            - ax * self.coro_keep_alive["serial_listener_2"][1] * 0.005,
-                                            4,
-                                        )
-                                        temp_for_offz["y"] = round(
-                                            past_velocity[1]
-                                            - ay * self.coro_keep_alive["serial_listener_2"][1] * 0.005,
-                                            4,
-                                        )
-                                        temp_for_offz["z"] = round(
-                                            past_velocity[2]
-                                            - az * self.coro_keep_alive["serial_listener_2"][1] * 0.005,
-                                            4,
-                                        )
-                                        u.rotate_y(
-                                            {"": temp_for_offz}, math.radians(yaw_offset),
-                                        )
+                            # if velocity_until_reset < 20:
 
-                                        if self.useVelocity:
-                                            self.temp_pose.vel_x = temp_for_offz["x"]
-                                            self.temp_pose.vel_y = temp_for_offz["y"]
-                                            self.temp_pose.vel_z = temp_for_offz["z"]
+                            #     u.rotate_y(
+                            #         {"": temp_for_offz}, math.radians(-yaw_offset),
+                            #     )
+                            #     past_velocity = [
+                            #         temp_for_offz["x"],
+                            #         temp_for_offz["y"],
+                            #         temp_for_offz["z"],
+                            #     ]
+                            #     temp_for_offz["x"] = round(
+                            #         past_velocity[0]
+                            #         - ax * self.coro_keep_alive["serial_listener_2"][1] * 0.005,
+                            #         4,
+                            #     )
+                            #     temp_for_offz["y"] = round(
+                            #         past_velocity[1]
+                            #         - ay * self.coro_keep_alive["serial_listener_2"][1] * 0.005,
+                            #         4,
+                            #     )
+                            #     temp_for_offz["z"] = round(
+                            #         past_velocity[2]
+                            #         - az * self.coro_keep_alive["serial_listener_2"][1] * 0.005,
+                            #         4,
+                            #     )
+                            #     u.rotate_y(
+                            #         {"": temp_for_offz}, math.radians(yaw_offset),
+                            #     )
 
-                                        velocity_until_reset += 1
+                            #     if self.useVelocity:
+                            #         self.temp_pose.vel_x = temp_for_offz["x"]
+                            #         self.temp_pose.vel_y = temp_for_offz["y"]
+                            #         self.temp_pose.vel_z = temp_for_offz["z"]
 
-                                    else:
-                                        velocity_until_reset = 0
-                                        temp_for_offz = {"x": 0, "y": 0, "z": 0}
-                                        self.temp_pose.vel_x = 0
-                                        self.temp_pose.vel_y = 0
-                                        self.temp_pose.vel_z = 0
+                            #     velocity_until_reset += 1
 
-                                    yaw = y
-                                    eulers = [y + yaw_offset, p, r]
-                                    eulers = np.radians(eulers)
-                                    x, y, z, w = pyrr.Quaternion.from_eulers(eulers).xyzw
+                            # else:
+                            #     velocity_until_reset = 0
+                            #     temp_for_offz = {"x": 0, "y": 0, "z": 0}
+                            #     self.temp_pose.vel_x = 0
+                            #     self.temp_pose.vel_y = 0
+                            #     self.temp_pose.vel_z = 0
 
-                                    self.temp_pose.r_w = round(w, 4)
-                                    self.temp_pose.r_x = round(y, 4)
-                                    self.temp_pose.r_y = round(-x, 4)
-                                    self.temp_pose.r_z = round(-z, 4)
+                            my_q = Quaternion([-y, z, -x, w])
 
-                                    self.temp_pose.trigger_value = abs(trgr - 1)
-                                    self.temp_pose.grip = abs(grp - 1)
-                                    self.temp_pose.menu = abs(menu - 1)
-                                    self.temp_pose.system = abs(sys - 1)
-                                    self.temp_pose.trackpad_click = abs(padClk - 1)
+                            my_q = my_off * my_q
+                            self.temp_pose.r_w = round(my_q[3], 5)
+                            self.temp_pose.r_x = round(my_q[0], 5)
+                            self.temp_pose.r_y = round(my_q[1], 5)
+                            self.temp_pose.r_z = round(my_q[2], 5)
 
-                                    self.temp_pose.trackpad_x = round((padX - 524) / 530, 3) * (-1)
-                                    self.temp_pose.trackpad_y = round((padY - 506) / 512, 3) * (-1)
+                            self.temp_pose.trigger_value = trgr
+                            self.temp_pose.grip = grp
+                            self.temp_pose.menu = menu
+                            self.temp_pose.system = sys
+                            self.temp_pose.trackpad_click = padClk
 
-                                    temp_mode = abs(util - 1)
+                            self.temp_pose.trackpad_x = round((padX - 428) / 460, 3) * (-1)
+                            self.temp_pose.trackpad_y = round((padY - 530) / 540, 3) * (-1)
 
-                                    self._serialResetYaw = False
-                                    if self.temp_pose.trackpad_x > 0.6 and temp_mode:
-                                        self.mode = 1
+                            self._serialResetYaw = False
+                            if self.temp_pose.trackpad_x > 0.6 and util:
+                                self.mode = 1
 
-                                    elif self.temp_pose.trackpad_x < -0.6 and temp_mode:
-                                        self.mode = 0
+                            elif self.temp_pose.trackpad_x < -0.6 and util:
+                                self.mode = 0
 
-                                    elif self.temp_pose.trackpad_y < -0.6 and temp_mode:
-                                        self.useVelocity = False
+                            elif self.temp_pose.trackpad_y < -0.6 and util:
+                                self.useVelocity = False
 
-                                    elif self.temp_pose.trackpad_y > 0.6 and temp_mode:
-                                        self.useVelocity = True
+                            elif self.temp_pose.trackpad_y > 0.6 and util:
+                                self.useVelocity = True
 
-                                    elif temp_mode:
-                                        yaw_offset = 0 - yaw
-                                        self._serialResetYaw = True
+                            elif util:
+                                my_off = ~Quaternion([0, z, 0, w])
+                                self._serialResetYaw = True
 
-                                if abs(self.temp_pose.trackpad_x) > 0.07 or abs(self.temp_pose.trackpad_y) > 0.07:
-                                    self.temp_pose.trackpad_touch = 1
+                        if abs(self.temp_pose.trackpad_x) > 0.09 or abs(self.temp_pose.trackpad_y) > 0.09:
+                            self.temp_pose.trackpad_touch = 1
 
-                                else:
-                                    self.temp_pose.trackpad_touch = 0
+                        else:
+                            self.temp_pose.trackpad_touch = 0
 
-                                if self.temp_pose.trigger_value > 0.9:
-                                    self.temp_pose.trigger_click = 1
+                        if self.temp_pose.trigger_value > 0.9:
+                            self.temp_pose.trigger_click = 1
 
-                                else:
-                                    self.temp_pose.trigger_click = 0
+                        else:
+                            self.temp_pose.trigger_click = 0
 
-                                await asyncio.sleep(self.coro_keep_alive["serial_listener_2"][1])
+                        await asyncio.sleep(self.coro_keep_alive["serial_listener_2"][1])
 
-                            except Exception as e:
-                                print(f"{self.serial_listener_2.__name__}: {e}")
-                                break
-
-            except Exception as e:
-                print(f"serial_listener_2: {e}")
-
-            await asyncio.sleep(1)
+                    except Exception as e:
+                        print(f"{self.serial_listener_2.__name__}: {e}")
+                        break
 
     @templates.thread_register(1 / 100)
     async def serial_listener(self):
         """Get orientation data from serial."""
-        while self.coro_keep_alive["serial_listener"][0]:
-            try:
-                with serial.Serial(self.serialPaths["blue"], 115200, timeout=1 / 5) as ser2:
-                    with serial.threaded.ReaderThread(ser2, u.SerialReaderFactory) as protocol:
-                        yaw_offset = 0
-                        for _ in range(10):
-                            protocol.write_line("nut")
-                            await asyncio.sleep(1)
+        my_off = Quaternion()
+        with serial.Serial(self.serialPaths["blue"], 115200, timeout=1 / 5) as ser2:
+            with serial.threaded.ReaderThread(ser2, u.SerialReaderFactory) as protocol:
+                protocol.write_line("nut")
+                await asyncio.sleep(5)
 
-                        while self.coro_keep_alive["serial_listener"][0]:
-                            try:
-                                gg = u.get_numbers_from_text(protocol.last_read)
+                while self.coro_keep_alive["serial_listener"][0]:
+                    try:
+                        gg = u.get_numbers_from_text(protocol.last_read, ',')
 
-                                if len(gg) > 0:
-                                    ypr = gg
+                        if len(gg) > 0:
+                            w, x, y, z = gg
+                            my_q = Quaternion([-y, z, -x, w])
 
-                                    eulers = [ypr[0] + yaw_offset, ypr[1], ypr[2]]
-                                    eulers = np.radians(eulers)
-                                    x, y, z, w = pyrr.Quaternion.from_eulers(eulers).xyzw
+                            if self._serialResetYaw:
+                                my_off = ~Quaternion([0, z, 0, w])
 
-                                    # self.pose['rW'] = rrr2[0]
-                                    # self.pose['rX'] = -rrr2[2]
-                                    # self.pose['rY'] = rrr2[3]
-                                    # self.pose['rZ'] = -rrr2[1]
+                            my_q = my_off * my_q
+                            self.pose.r_w = round(my_q[3], 5)
+                            self.pose.r_x = round(my_q[0], 5)
+                            self.pose.r_y = round(my_q[1], 5)
+                            self.pose.r_z = round(my_q[2], 5)
 
-                                    self.pose.r_w = round(w, 4)
-                                    self.pose.r_x = round(y, 4)
-                                    self.pose.r_y = round(-x, 4)
-                                    self.pose.r_z = round(-z, 4)
+                        await asyncio.sleep(self.coro_keep_alive["serial_listener"][1])
 
-                                    if self._serialResetYaw:
-                                        yaw_offset = 0 - ypr[0]
-
-                                await asyncio.sleep(self.coro_keep_alive["serial_listener"][1])
-
-                            except Exception as e:
-                                print(f"{self.serial_listener.__name__}: {e}")
-                                break
-
-            except Exception as e:
-                print(f"serial_listener: {e}")
-
-            await asyncio.sleep(1)
-
+                    except Exception as e:
+                        print(f"{self.serial_listener.__name__}: {e}")
+                        break
 
 def run_poser_only(addr="127.0.0.1", cam=4, colordata=None, mapdata=None):
     """Run the poser only. The server must be started in another program."""
