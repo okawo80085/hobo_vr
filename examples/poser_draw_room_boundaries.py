ded3b847cc9ba22b60581d2faf8034c1a1db6c6b
okawo <okawo.198@gmail.com>
2020-07-09 18:51:41 +0300

    Update poser_draw_room_boundaries.py

--- examples/poser_draw_room_boundaries.py
+++ examples/poser_draw_room_boundaries.py
@@ -1,3 +1,14 @@
+'''
+
+first positions both controllers to ROOM_CENTER then waits for resume
+
+then draws a box starting at START_POS with STEP*STEP_MUL for NUM_ITER times for each element of APPLY_DIM
+
+needs aioconsole module installed
+
+'''
+
+
 
 import asyncio
 import time
@@ -6,12 +17,16 @@ import pyrr
 
 from virtualreality import templates
 from virtualreality.server import server
+from aioconsole import ainput
 
 poser = templates.PoserClient()
 
-START_POS = np.array([0, 0, 0], dtype=np.float64) # [x, y, z]
+START_POS = np.array([2, -0.5, 2], dtype=np.float64) # [x, y, z]
 
-STEP = 0.5
+ROOM_CENTER = np.array([0, -2, 1.5], dtype=np.float64) # [x, y, z]
+
+
+STEP = 0.1
 STEP_MUL = -1
 
 NUM_ITER = 20
@@ -21,7 +36,7 @@ APPLY_DIM = np.array([
             [0, 0, 1],
             [-1, 0, 0],
             [0, 0, -1],
-            ], dtype=np.float64)
+            ], dtype=np.float64) # this dictates the shape of the boundary
 
 np.set_printoptions(formatter={'all':lambda x:'{:+.4f}, '.format(x)})
 
@@ -29,12 +44,26 @@ np.set_printoptions(formatter={'all':lambda x:'{:+.4f}, '.format(x)})
 @poser.thread_register(1/30)
 async def example_thread():
     global START_POS
+
+    poser.pose_controller_r.x = ROOM_CENTER[0]
+    poser.pose_controller_r.y = ROOM_CENTER[1]
+    poser.pose_controller_r.z = ROOM_CENTER[2]
+
+    poser.pose_controller_l.x = ROOM_CENTER[0]
+    poser.pose_controller_l.y = ROOM_CENTER[1]
+    poser.pose_controller_l.z = ROOM_CENTER[2]
+
+    await ainput('controllers at ROOM_CENTER, press enter to continue...')
+
+    await asyncio.sleep(1)
+
+    print ('starting boundary draw...')
+
     poser.pose_controller_r.trigger_value = 1
     poser.pose_controller_r.trigger_click = 1
 
     poser.pose_controller_l.trigger_value = 1
     poser.pose_controller_l.trigger_click = 1
-    print ('starting boundary draw...')
 
     for i in APPLY_DIM:
         for _ in range(NUM_ITER):
