0b679fd2ff16740f2fae28d945490685269a072c
sahasam <munamalasahas@gmail.com>
2020-07-19 21:27:38 -0700

    add logging capabilites to server module

--- virtualreality/server/__init__.py
+++ virtualreality/server/__init__.py
@@ -1,4 +1,27 @@
-"""Server lol."""
+"""Virtualreality Pose Server"""
+import logging
+import os
 from . import server
 
 __version__ = "0.1"
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+#logfile
+log_path = os.path.join(os.path.dirname(__file__), "../logs/server.log")
+logger_handler = logging.FileHandler(log_path)
+logger_handler.setLevel(logging.DEBUG)
+
+#console logging
+console_logger_handler = logging.StreamHandler()
+console_logger_handler.setLevel(logging.INFO)
+
+#logging entry format
+logger_formatter = logging.Formatter('[%(asctime)s] %(name)s %(levelname)s - %(message)s')
+
+logger_handler.setFormatter(logger_formatter)
+console_logger_handler.setFormatter(logger_formatter)
+
+logger.addHandler(logger_handler)
+logger.addHandler(console_logger_handler)
--- virtualreality/server/__main__.py
+++ virtualreality/server/__main__.py
@@ -2,12 +2,10 @@
 pyvr server.
 
 usage:
-  server [--show-messages]
+  server [options]
 
 options:
     -h --help               shows this message
-    -d --show-messages      show messages
-
 """
 from virtualreality.server import server
 
@@ -16,7 +14,5 @@ from . import __version__
 from docopt import docopt
 
 args = docopt(__doc__, version=__version__)
-
-server.PRINT_MESSAGES = args["--show-messages"]
 
 server.run_til_dead()
