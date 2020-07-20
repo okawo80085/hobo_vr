ebe94ccc87b226381d439fda9d5b01f624e3500a
sahasam <munamalasahas@gmail.com>
2020-07-19 21:33:45 -0700

    add logging capabilites to calibration module

--- virtualreality/calibration/__init__.py
+++ virtualreality/calibration/__init__.py
@@ -1,1 +1,14 @@
 """Calibration functions."""
+import logging
+import os
+
+logger = logging.getLogger(__file__)
+logger.setLevel(logging.INFO)
+
+log_file = os.path.join(os.path.dirname(__file__), "../logs/calibration.log")
+logger_handler = logging.FileHandler(log_file)
+
+logger_formatter = logging.Formatter('[%(asctime)s] %(name)s %(levelname)s - %(message)s')
+
+logger_handler.setFormatter(logger_formatter)
+logger.addHandler(logger_handler)
