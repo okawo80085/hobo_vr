b93f2299c7c126d31b3ec61820db7bee3dd26d21
okawo <okawo.198@gmail.com>
2020-08-25 15:00:26 +0300

    ups typo

--- driver_unregister_win.bat
+++ driver_unregister_win.bat
@@ -3,6 +3,6 @@ IF EXIST "C:\Program Files (x86)\Steam\steamapps\common\SteamVR\bin\win32\" (
 "C:\Program Files (x86)\Steam\steamapps\common\SteamVR\bin\win32\vrpathreg.exe" removedriver "%~dp0hobovr"
 "C:\Program Files (x86)\Steam\steamapps\common\SteamVR\bin\win32\vrpathreg.exe" show
 ) ELSE (
-echo "SteamVR not located in C:\\Program Files (x86)\\Steam\\steamapps\\common\\SteamVR - Installation Failed"
+echo "SteamVR not located in C:\\Program Files (x86)\\Steam\\steamapps\\common\\SteamVR - Uninstallation Failed"
 )
 pause
