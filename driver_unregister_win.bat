2aa5da6d28918c6503f3d6fd0eedaf26e04ea9ab
okawo <okawo.198@gmail.com>
2020-08-25 14:47:58 +0300

    lets face it
    windows users don't know how to use command line tools

---
+++ driver_unregister_win.bat
@@ -0,0 +1,8 @@
+@echo off
+IF EXIST "C:\Program Files (x86)\Steam\steamapps\common\SteamVR\bin\win32\" (
+"C:\Program Files (x86)\Steam\steamapps\common\SteamVR\bin\win32\vrpathreg.exe" removedriver "%~dp0hobovr"
+"C:\Program Files (x86)\Steam\steamapps\common\SteamVR\bin\win32\vrpathreg.exe" show
+) ELSE (
+echo "SteamVR not located in C:\\Program Files (x86)\\Steam\\steamapps\\common\\SteamVR - Installation Failed"
+)
+pause
