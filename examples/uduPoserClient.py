49b0f846153390e80f2b6a60521062f5d9726074
okawo <okawo.198@gmail.com>
2020-09-16 13:41:01 +0300

    added an example haptic receive thread

--- examples/uduPoserClient.py
+++ examples/uduPoserClient.py
@@ -12,7 +12,7 @@ import pyrr
 from virtualreality import templates
 from virtualreality.server import server
 
-poser = templates.UduPoserClient("h c c")  # devices setup identical to normal posers
+poser = templates.UduPoserClient("h c c t t")  # devices setup identical to normal posers
 
 
 @poser.thread_register(1 / 60)
@@ -33,6 +33,17 @@ async def example_thread():
         h += 0.01
 
         await asyncio.sleep(poser.coro_keep_alive["example_thread"].sleep_delay)
+
+
+@poser.thread_register(1 / 60)
+async def example_receive_haptics_thread():
+    while poser.coro_keep_alive["example_receive_haptics_thread"].is_alive:
+        if poser.last_read:
+            print (poser.last_read)
+            poser.last_read = b""
+
+        await asyncio.sleep(poser.coro_keep_alive["example_receive_haptics_thread"].sleep_delay)
+
 
 
 asyncio.run(poser.main())
