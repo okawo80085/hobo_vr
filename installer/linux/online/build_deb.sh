90d654d78befaac26df7dd5b36e5070e6dbabbec
okawo80085 <okawo.198@gmail.com>
2021-07-31 09:37:56 +0300

    installers + driver build
    added deps to both linux installers
    
    added online linux installer, it installs the driver from master
    
    and added the instructions to use and build the said installers

---
+++ installer/linux/online/build_deb.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# gotta make sure its in /linux/offline
+# it relies on relative paths from there
+if [ ! -f "./hobo_vr/DEBIAN/postinst" ]; then
+    echo "this script should be run from its directory"
+    exit 1
+fi
+
+# echo "copy driver into package..."
+# cp -r ../../../hobovr ./hobo_vr/usr/local/share/
+
+dpkg-deb --build hobo_vr
+
+# echo "cleanup..."
+# rm -rf ./hobo_vr/usr/local/share/hobovr
+echo "done"
