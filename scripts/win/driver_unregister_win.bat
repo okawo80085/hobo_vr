94fc61a1a9e6955d93151d2b07b5056a1a3ed9c6
okawo <okawo.198@gmail.com>
2021-08-01 13:40:03 +0300

    windows batch file is garbage

--- scripts/win/driver_unregister_win.bat
+++ scripts/win/driver_unregister_win.bat
@@ -1,7 +1,19 @@
 @echo off
 IF EXIST "C:\Program Files (x86)\Steam\steamapps\common\SteamVR\bin\win32\" (
-"C:\Program Files (x86)\Steam\steamapps\common\SteamVR\bin\win32\vrpathreg.exe" removedriver "%~dp0hobovr"
-"C:\Program Files (x86)\Steam\steamapps\common\SteamVR\bin\win32\vrpathreg.exe" show
+IF EXIST "%~dp0..\..\hobovr" (
+	set old_pwd="%~dp0"
+	cd  "..\..\hobovr"
+
+rem fuck windows, this shit is disgusting, you need a fucking for loop to capture an output of a command wtf
+	FOR /F "tokens=* USEBACKQ" %%F IN (`cd`) DO (
+	SET var=%%F
+	)
+	cd %old_pwd%
+	"C:\Program Files (x86)\Steam\steamapps\common\SteamVR\bin\win32\vrpathreg.exe" removedriver "%var%"
+	"C:\Program Files (x86)\Steam\steamapps\common\SteamVR\bin\win32\vrpathreg.exe" show
+) ELSE (
+	echo "could not find driver, is the repository broken?"
+)
 ) ELSE (
 echo "SteamVR not located in C:\\Program Files (x86)\\Steam\\steamapps\\common\\SteamVR - Uninstallation Failed"
 )
