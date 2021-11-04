c0d84853eb219860d2fd01ec5525b3c3f7325ea4
Oleg Vorobiov <okawo.198@gmail.com>
2021-11-05 01:59:21 +0200

    driver: src: Removed old code and scripts
    Removed old unused or obsolete code and scripts

--- driver/src/after_compile_win.bat
+++
@@ -1,8 +0,0 @@
-cd ..\..\hobovr\bin\win64
-cd
-xcopy ..\..\..\driver\src\bin\win64\driver_hobovr.dll
-cd ..\..\..\driver\src
-cd ..\..\hobovr\bin\win32
-cd
-xcopy ..\..\..\driver\src\bin\win32\driver_hobovr.dll
-cd ..\..\..\driver\src
